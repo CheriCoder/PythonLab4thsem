f55763514c1bf8af1661479be71d4ae50ce7ee46
CheriCoder <120724271+CheriCoder@users.noreply.github.com>
2023-06-16 14:55:59 +0530

    Add files via upload
    16 june - avg of internal marks and palindrome program

---
+++ 1.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+2+3*6
+
+
+# In[2]:
+
+
+(2+3)*6
+
+
+# In[3]:
+
+
+2**8
+
+
+# In[4]:
+
+
+23/7
+
+
+# In[5]:
+
+
+(5-1)*((7+1)/(3-1))
+
+
+# In[8]:
+
+
+print ('what is your name?')
+print ('joe')
+print ('joe who?')
+print('joe mama')
+
+
+# In[14]:
+
+
+'yamete kudasai oni chan >_<'
+
+
+# In[16]:
+
+
+'sasriakal'+'paji'
+
+
+# In[19]:
+
+
+'wei nihao ,wode mingze shi lu zhao yao'
+
+
+# In[20]:
+
+
+'hahaha'*5
+
+
+# 
+
+# In[23]:
+
+
+idris = 60
+shashank = 9
+idris +shashank
+
+
+# 
+
+# In[39]:
+
+
+'69 '* 5**5
+
+
+# In[31]:
+
+
+byuntai='gattu'
+byuntai
+
+
+# In[32]:
+
+
+byuntai = 'idris'
+byuntai
+
+
+# In[33]:
+
+
+kutta_kamina = 'bc'
+kutta_kamina
+
+
+# In[41]:
+
+
+sussy_baka='idris kun'
+sussy_baka
+
+
+# In[51]:
+
+
+print ('wei nihao')
+print('ni shama mingze ?')
+myName=input()
+print ( myName + '\t hao piao liang de mingze')
+print ( myName + '\t ni de ninaling shama?')
+myAge = input()
+print ('hen gaoxing renshi ni, shungdi')
+print ('mingnian\t' + myName + '\tnianling shi\t'+ str(int(myAge)+1))
+
+
+# In[50]:
+
+
+print ('henloo')
+print('yo name ?')
+myName=input()
+print ( myName + '\t noice name')
+print ( myName + '\t yo age?')
+myAge = input()
+print ('nice to meet ya bro')
+print ('next year \t' + myName + '\t will be \t'+ str(int(myAge)+1))
+
+
+# In[52]:
+
+
+str (-3.14)
+#converting int to string
+
+
+# In[65]:
+
+
+int('69')
+#converting string to int
+
+
+# In[54]:
+
+
+float ('3.14')
+#converting string to float
+
+
+# In[55]:
+
+
+float (-10)
+#converting int to float
+
+
+# In[57]:
+
+
+spam = input()
+spam
+
+
+# In[59]:
+
+
+spam = int(spam)
+spam
+
+
+# In[61]:
+
+
+spam *10/5
+
+
+# In[63]:
+
+
+42 == '42'
+# basically int is not equal to float
+
+
+# In[64]:
+
+
+42 == 42.00
+#but int = float
+
+
+# In[67]:
+
+
+bacon = 20
+bacon +1
+
+
+# In[68]:
+
+
+'spam' + 'spamspam'
+
+
+# In[69]:
+
+
+'spam' *3
+
+
+# In[ ]:
+
+
+
+
